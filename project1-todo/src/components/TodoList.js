4023da1e05aaa18eef410f785f3d3217fe7b6d9d
Damodar Lohani <dlohani48@gmail.com>
2019-03-14 07:25:37 +0545

    no tasks message

--- project1-todo/src/components/TodoList.js
+++ project1-todo/src/components/TodoList.js
@@ -3,8 +3,8 @@ import React from 'react'
 const TodoList = ({todos, deleteTodo}) => {
   return (
     <div className="collection">
-     { todos.map(todo => 
-        <p key={todo.id} onClick={(e)=>deleteTodo(todo.id)} className="collection-item">{todo.content}</p>)}
+     { todos.length > 0 ? todos.map(todo => 
+        <p key={todo.id} onClick={(e)=>deleteTodo(todo.id)} className="collection-item">{todo.content}</p>) : (<p>You have completed all your tasks. Yay!</p>)}
     </div>
   )
 }
