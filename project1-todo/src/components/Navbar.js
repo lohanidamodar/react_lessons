270166968bc0a14473482300b074d948cc5f20e1
Damodar Lohani <dlohani48@gmail.com>
2019-03-15 10:28:19 +0545

    Active link using NavLink

--- project1-todo/src/components/Navbar.js
+++ project1-todo/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = (props) => {
   return (
@@ -8,8 +8,8 @@ const Navbar = (props) => {
         <a href="/" className="left brand-logo">Todo App</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
       </div>
     </nav>
