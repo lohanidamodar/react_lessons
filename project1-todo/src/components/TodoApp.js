9776f67bc9778d7f66b0b0659d4d52bb74f14e9c
Damodar Lohani <dlohani48@gmail.com>
2019-04-01 09:00:00 +0545

    route params

--- project1-todo/src/components/TodoApp.js
+++ project1-todo/src/components/TodoApp.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
+import TodoDetails from '../pages/TodoDetails';
 
 const TodoApp = (props) => {
     return (
@@ -15,6 +16,7 @@ const TodoApp = (props) => {
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+          <Route path="/todo/:todoId" component={TodoDetails} />
         </div>
       </BrowserRouter> 
     );
--- project1-todo/src/components/TodoList.js
+++ project1-todo/src/components/TodoList.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const TodoList = ({todos, deleteTodo}) => {
   return (
     <div className="collection">
-     { todos.length > 0 ? todos.map(todo => 
-        <p key={todo.id} onClick={(e)=>deleteTodo(todo.id)} className="collection-item">{todo.content}</p>) : (<p>You have completed all your tasks. Yay!</p>)}
+     { todos.length > 0 
+        ? todos.map(todo => 
+            <p key={todo.id} onClick={(e)=>deleteTodo(todo.id)} className="collection-item">
+            {todo.content} 
+            <Link to={'/todo/' + todo.id}>
+              <span className="waves-effect waves-light btn">View</span>
+            </Link>
+            </p>
+          )
+          : (<p>You have completed all your tasks. Yay!</p>)}
     </div>
   )
 }
